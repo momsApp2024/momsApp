2327e83bab7f8a959d26b038aa5614b79704fb09
developer <developer@busoft.co.il>
2024-07-02 01:38:36 +0300

    initial

---
+++ payrollCalculatorApi/Models/SalaryCalculationResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace payrollCalculatorApi.Models
+{
+    public class SalaryCalculationResponse
+    {
+        public decimal BasicSalary { get; set; }
+        public decimal SeniorityIncrementRate { get; set; }
+        public decimal TotalSeniorityAddition { get; set; }
+        public decimal TotalOvertimeAddition { get; set; }
+        public decimal TotalBaseSalaryBeforeIncrease { get; set; }
+        public decimal SalaryIncreaseRate { get; set; }
+        public decimal TotalSalaryIncrease { get; set; }
+        public decimal TotalBaseSalaryAfterIncrease { get; set; }
+    }
+}
---
+++ payrollCalculatorApi/Program.cs
@@ -0,0 +1,75 @@
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+
+//builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
+
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        // Configure JSON options if necessary
+        options.JsonSerializerOptions.PropertyNamingPolicy = null; // Example: Keep property names as they are
+    });
+
+// Configure CORS to allow requests from the Angular app
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowAllOrigins");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
