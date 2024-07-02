c0470f9e7349f79dfb4ffe3ef590edb99901d7e6
developer <developer@busoft.co.il>
2024-07-02 07:11:33 +0300

    finalcommit

--- payrollCalculatorApi/Controllers/SalaryCalculatorController.cs
+++ payrollCalculatorApi/Controllers/SalaryCalculatorController.cs
@@ -1,5 +1,4 @@
-﻿
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using payrollCalculatorApi.Models;
 namespace payrollCalculatorApi.Controllers
 {
--- payrollCalculatorApi/Program.cs
+++ payrollCalculatorApi/Program.cs
@@ -1,45 +1,14 @@
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-
-//builder.Services.AddControllers();
-//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-//app.UseHttpsRedirection();
-
-//app.UseAuthorization();
-
-//app.MapControllers();
-
-//app.Run();
-
-
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// add services to the container.
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
-        // Configure JSON options if necessary
-        options.JsonSerializerOptions.PropertyNamingPolicy = null; // Example: Keep property names as they are
+        // configure JSON options if necessary
+        options.JsonSerializerOptions.PropertyNamingPolicy = null; 
     });
 
-// Configure CORS to allow requests from the Angular app
+// configure CORS to allow requests from the Angular app
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAllOrigins",
@@ -51,13 +20,12 @@ builder.Services.AddCors(options =>
         });
 });
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
